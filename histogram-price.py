958c7e9d41a65b4ff851c38391a868c7bb288077
Alessandro Hamuche <58093537+afhamuche@users.noreply.github.com>
2022-10-15 09:05:17 -0300

    Create histogram-price.py

---
+++ histogram-price.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import csv
+
+filename = "historical_1665783247-1665178447.csv"
+pricefile = "price_" + filename
+
+
+for x in range(2):
+    if x==0:
+        awtype = 'w'
+    else:
+        awtype = 'a'
+
+    with open('histogram_' + pricefile, awtype, newline='') as histogram:
+        histogramobj = csv.writer(histogram)
+
+        with open(pricefile, 'r', newline='') as readfile:
+            readobj = csv.reader(readfile)
+            header = next(readobj)
+            header = header[3:]
+            header_max = header[-1]
+            header_size = len(header)
+            histogramobj.writerow(header)
+
+            aList = []
+            for i in range(header_size):
+                aList.append(0)
+
+            for row in readobj:
+                aBool = row[0] == "True"
+                print(aBool)
+                print("NOT A BOOL " + str(not aBool))
+                tmpList = row[3:]
+                if aBool and awtype == 'w':
+                    placement = tmpList.index('True')
+                    aList[placement] += 1
+                elif awtype == 'a' and not aBool:
+                    placement = tmpList.index('True')
+                    aList[placement] += 1
+
+            histogramobj.writerow(aList)
+
